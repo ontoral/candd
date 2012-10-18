45d818b8e48865591269f02bd61aaac4bbcf9beb
Marcus M. Darden <marcus.darden@cs.olivetcollege.edu>
2012-10-18 11:15:46 -0400

    Tweaks for a specific download range.
    This file needs to built to run interactively, so as to eliminate the need
    for constant editing of the source.

--- dev/indexer.py
+++ dev/indexer.py
@@ -4,28 +4,14 @@ import pricer
 
 
 if __name__ == '__main__':
-    index_file = os.path.join('..', 'indexes.txt')
-    price_dir = os.path.join('..', 'indexes')
+	index_file = 'indexes.txt'
 
-    start_date = datetime.date(2001, 1, 1).toordinal()
-    end_date = datetime.date.today().toordinal()
-    end_date = datetime.date(2003, 1, 1).toordinal()
+	start_date = datetime.date(2001, 1, 1).toordinal()
+	end_date = datetime.date.today().toordinal()
 
-    month = datetime.date.fromordinal(start_date).month
-    for day in range(start_date, end_date):
-        dt = datetime.date.fromordinal(day)
-        if dt.weekday() >= 5:
-            # Never download a Saturday or Sunday
-            continue
-        if dt.weekday() == 4:
-            # Skip Friday if it isn't last Friday of the month
-            monday = datetime.date.fromordinal(day + 3)
-            if dt.month == monday.month:
-                continue
-        else:
-            # Skip a Monday-Thursday if the next day is same month
-            tomorrow = datetime.date.fromordinal(day + 1)
-            if dt.month == tomorrow.month:
-                continue
-        pricer.get_quotes(dt.month, dt.day, dt.year, index_file, price_dir)
+	for day in range(start_date, end_date):
+		dt = datetime.date.fromordinal(day)
+		if dt.weekday() >= 5:
+			continue
+		pricer.get_quotes(dt.month, dt.day, dt.year, 'indexes.txt', 'indexes')
 
