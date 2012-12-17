4b192a694b5f42b4ad4c11d01522799a8713a03a
Marcus M. Darden <admin@chisholmanddames.com>
2012-12-17 17:23:04 -0500

    Parsing files tests.

--- bin/schwabber.py
+++ bin/schwabber.py
@@ -3,9 +3,10 @@
 import os
 import csv
 import glob
+import re
 
 
-PATH = '.'
+SCHWAB_EXPORTS = '.'
 
 class SchwabDialect(csv.Dialect):
     def __init__(self):
@@ -13,5 +14,35 @@ class SchwabDialect(csv.Dialect):
         self.lineterminator = '\r\n'
         self.quotechar = '"'
         self.quoting = csv.QUOTE_ALL
+csv.register_dialect('schwab_export', SchwabDialect())
 
+trans = os.path.join(os.environ.get('SCHWAB_EXPORTS', SCHWAB_EXPORTS),
+                     '*_Transactions_*.CSV')
+pos = os.path.join(os.environ.get('SCHWAB_EXPORTS', SCHWAB_EXPORTS),
+                   '*_Positions_*.CSV')
 
+def files_handler(path):
+    for tt in glob.iglob(path):
+        with file(tt, 'r') as f:
+            prefix = os.path.basename(tt).split('-')[0]
+            if int(prefix) == 0:    # a closed account
+                continue
+            info = f.readline()
+            acct_re = re.search('^.*XXXX(-[0-9]{4}).*', info)
+            suffix = acct_re.groups()[0]
+            account_num = prefix+suffix
+            print account_num
+            csv_reader = csv.reader(f, dialect='schwab_export')
+            header = None
+            for row in csv_reader:
+                if not header:
+                    header = row
+                else:
+                    if len(row) != len(header):
+                        continue
+                    d = dict(zip(header, row))
+                    d['account'] = account_num
+                    print d
+
+files_handler(trans)
+files_handler(pos)
